45fa8b858fcad8c87e088c573d71c946d40a91c8
TelesAo <69825229+TelesAo@users.noreply.github.com>
2020-10-28 20:19:30 -0300

    Update Poligono.php

--- noobframework-master/App/Models/Poligono.php
+++ noobframework-master/App/Models/Poligono.php
@@ -6,8 +6,8 @@ use Core\DataBase;
    Class Poligono{
 
     public static function getAll($condition = null, $columns = null){
-        $db= DataBase::getInstance('tb_poligono', 'nome_poligono', 'lados_poligono','angulo_poligono');
-
+        //$db= DataBase::getInstance('tb_poligono', 'nome_poligono', 'lados_poligono','angulo_poligono');
+         $db= DataBase::getInstance();
         //var_dump($db->getList('tb_poligono', '*'));
         //die();
         
